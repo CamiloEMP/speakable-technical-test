c89dea540148ca81d510d11864205943cbbccfcf
CamiloEPz <morapaezc@gmail.com>
2024-04-18 20:59:29 -0500

    Integrate Canvas LMS API with Firebase services

--- src/models/canvas-lms/courses.model.ts
+++ src/models/canvas-lms/courses.model.ts
@@ -3,8 +3,11 @@ import { type CourseModelType } from "@/schemas/courses.schemas"
 
 import { canvasLmsApi } from "./canvas-lms.model"
 import { buildPath } from "@/utils/build-path.utils"
+import { createUsersFromSectionsCanvasLMS } from "@/lib/firebase/services/create-users-from-sections-canvas-lms.service"
+import { createSectionCanvasLMS } from "@/lib/firebase/services/create-section-canvas-lms.service"
 
 export class CourseModel implements CourseModelType {
+  // This method is only to test the integration with Canvas LMS API
   async getAllCourses() {
     try {
       const response = await canvasLmsApi.get<any[]>("/courses")
@@ -21,9 +24,14 @@ export class CourseModel implements CourseModelType {
         ...params
       })
 
-      const response = await canvasLmsApi.get<Section[]>(path)
+      const response = await canvasLmsApi.get<{ data: Section[] }>(path)
 
-      return response
+      const sections = response.data ?? []
+
+      await createSectionCanvasLMS(sections)
+      await createUsersFromSectionsCanvasLMS(sections)
+
+      return sections
     } catch (error) {
       throw error
     }
